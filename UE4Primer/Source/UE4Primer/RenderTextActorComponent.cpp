ba1a0b06bd1f47a0038d07ffd808516ad8170594
badlabor <badlabor@work.com>
2016-08-23 11:17:15 +0800

    格式化代码

--- UE4Primer/Source/UE4Primer/RenderTextActorComponent.cpp
+++ UE4Primer/Source/UE4Primer/RenderTextActorComponent.cpp
@@ -41,11 +41,11 @@ void URenderTextActorComponent::BeginPlay()
 		Canvas.DrawItem(line1);
 
 		// 绘制正方形
-		FVector2D cube[4];
-		cube[0] = FVector2D(10, 40);
-		cube[1] = FVector2D(110, 40);
-		cube[3] = FVector2D(10, 140);
-		cube[2] = FVector2D(110, 140);
+		FVector2D cube[] = { FVector2D(10, 40), FVector2D(110, 40), FVector2D(110, 140), FVector2D(10, 140) };
+// 		cube[0] = FVector2D(10, 40);
+// 		cube[1] = FVector2D(110, 40);
+// 		cube[3] = FVector2D(10, 140);
+// 		cube[2] = FVector2D(110, 140);
 		line1.SetColor(FLinearColor::Blue);
 		for (int i = 0; i < ARRAY_COUNT(cube); i++)
 		{
